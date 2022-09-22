0b03482faffbeca34d872c848aa3771b1c9449cd
Rohit Thakur <105488212+its-me-rohit@users.noreply.github.com>
2022-09-22 09:06:59 +0530

    Create Datatype.cpp

---
+++ Datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<iomanip> 
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl; 
+    cout<<fixed<<setprecision(3)<<e<<endl;
+    return 0;
+}
